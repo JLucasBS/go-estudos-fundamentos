d08525cba5fedda68ad82ad159dff6b2f31436d1
Joao Lucas <joao.santos@nappsolutions.com>
2023-10-03 16:42:12 +0000

    manipulacao basica de funcoes

---
+++ funcoes/main.go
@@ -0,0 +1,10 @@
+package main
+
+// Por estarem no mesmo pacote, esse aquivo consegue acessar as funções presente no arquivo funções
+
+// Para dar certo, rodar go run *.go
+
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
